d7d566c8d6739a98c30e7b200899e5a9b79bcbec
Thiago <thiagodsantos316@gmail.com>
2023-01-15 10:53:03 -0300

    Update index.jsx

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,16 +1,26 @@
 import Header from "../../components/Header";
 import "./Styles.css";
 
+import Phone from '../../assets/image/phone32.svg'
+import Social from '../../assets/image/redes-sociais-icon7.svg'
+import Pin from '../../assets/image/pin11.svg'
+import Nauber from '../../assets/image/logo-nauber@2x.png'
+import Retan from '../../assets/image/rectangle-141@2x.png'
+import Brasil from '../../assets/image/flagbrasil-219@2x.png'
+
 function Sobre(){
   return (
     <>
-    
     <div className="sobre-a-nauber2">
-      
+
       <div className="footer-div20">
         <div className="footer-div21">
           <div className="bg-div11" />
-          <img className="phone-icon32" alt="" src="src\assets\image\phone32.svg" />
+          <img 
+            className="phone-icon32" 
+            alt="" 
+            src={Phone}
+          />
           <div className="siga-nas-redes-sociais10">Siga nas redes sociais</div>
           <div className="nauber-2022-todos-os-direito10">
             Nauber © 2022 Todos os direitos reservados.
@@ -24,7 +34,7 @@ function Sobre(){
           <img
             className="redes-sociais-icon10"
             alt=""
-            src="src\assets\image\redes-sociais-icon7.svg"
+            src={Social}
           />
           <div className="dvidas-div10">
             <div className="converse-com-um-especialista10">
@@ -51,26 +61,49 @@ function Sobre(){
             <div className="div55">+55 (47) 98821-2725</div>
             <div className="nossa-sede-div21">Nossa sede</div>
           </div>
-          <img className="pin-icon11" alt="" src="src\assets\image\pin11.svg" />
+          <img 
+            className="pin-icon11" 
+            alt="" 
+            src={Pin} 
+          />
           <div className="line-div27" />
           <img
             className="logo-nauber-icon10"
             alt=""
-            src="src\assets\image\logo-nauber@2x.png"
+            src={Nauber}
           />
           <div className="rectangle-div266" />
         </div>
       </div>
-      <img className="rectangle-icon1" alt="" src="src\assets\image\rectangle-141@2x.png" />
+      <img 
+        className="rectangle-icon1" 
+        alt="" 
+        src={Retan} 
+        />
       <div className="mquinas-div6">
-        <img className="vector-icon62" alt="" src="src\assets\image\vector-1179.svg" />
-        <img className="vector-icon63" alt="" src="src\assets\image\vector-1169.svg" />
+        <img 
+          className="vector-icon62" 
+          alt="" 
+          src="src\assets\image\vector-1
+        179.svg" />
+        <img 
+          className="vector-icon63" 
+          alt="" 
+          src="src\assets\image\vector-1169.svg" />
         <b className="mquinas-para-pintura12">máquinas para pintura</b>
       </div>
       <div className="frame-div6">
         <div className="footer-div21">
-          <img className="bg-icon3" alt="" src="src\assets\image\bg@2x.png" />
-          <img className="nauber-pro-11" alt="" src="src\assets\image\nauber-pro-1@2x.png" />
+          <img 
+            className="bg-icon3" 
+            alt="" 
+            src="src\assets\image\bg@2x.png" 
+            />
+          
+          <img 
+            className="nauber-pro-11" 
+            alt="" 
+            src="src\assets\image\nauber-pro-1@2x.png" />
           <div className="rectangle-div268" />
           <div className="rectangle-div269" />
           <img
@@ -93,16 +126,38 @@ function Sobre(){
       </div>
       <div className="video-div6">
         <div className="rectangle-div270" />
-        <img className="play-icon6" alt="" src="src\assets\image\play6.svg" />
+        <img 
+          className="play-icon6" 
+          alt="" 
+          src="src\assets\image\play6.svg" 
+        />
       </div>
       <div className="group-div25">
         <img
           className="flag-brasil-2-icon19"
           alt=""
-          src="src\assets\image\flagbrasil-219@2x.png"
+          src={Brasil}
         />
         <b className="nauber-mquinas">nauber máquinas</b>
-        <div className="a-nauber-mquinas-uma-empres">{`A Nauber Máquinas é uma empresa Brasileira, sediada na cidade de Schroeder, Santa Catarina, a 8 km da cidade de  Jaraguá do Sul, e 25 km da Cidade de Joinville. Nosso objetivo é desenvolver e buscar novas tecnologias que proporcionem melhorias significativas de qualidade e desempenho de processos da construção civil. Com a globalização, e a disputa constante em um mercado cada vez mais acirrado, não há melhor forma de se tornar mais competitivo do que aprimorar processos para reduzir custos. Nossa linha de equipamentos Airless, Lixadeiras, Aspiradores e Acessórios, são desenvolvidas para profissionais que buscam otimização de mão de obra , e desempenhar produtividade sem abrir mão da qualidade. Os equipamentos e acessórios Nauber são produzidos sob total supervisão de qualidade, sendo que 100% dos equipamentos são testados antes do envio para nossos clientes, garantindo, dessa forma, a qualidade final, e a satisfação de compra através de equipamentos de ponta. `}</div>
+        <div className="a-nauber-mquinas-uma-empres">
+          {`A Nauber Máquinas é uma empresa Brasileira, 
+            sediada na cidade de Schroeder, Santa Catarina, 
+            a 8 km da cidade de  Jaraguá do Sul, 
+            e 25 km da Cidade de Joinville. 
+            Nosso objetivo é desenvolver e buscar novas tecnologias que 
+            proporcionem melhorias significativas de qualidade e desempenho de 
+            processos da construção civil. Com a globalização, 
+            e a disputa constante em um mercado cada vez mais acirrado, 
+            não há melhor forma de se tornar mais competitivo do que 
+            aprimorar processos para reduzir custos. Nossa linha de equipamentos 
+            Airless, Lixadeiras, Aspiradores e Acessórios, 
+            são desenvolvidas para profissionais que buscam otimização de 
+            mão de obra , e desempenhar produtividade sem abrir mão da qualidade. 
+            Os equipamentos e acessórios Nauber são produzidos sob total supervisão de qualidade, 
+            sendo que 100% dos equipamentos são testados antes do envio para nossos clientes, 
+            garantindo, dessa forma, a qualidade final, 
+            e a satisfação de compra através de equipamentos de ponta. `}
+            </div>
       </div>
       <Header />
     </div>
