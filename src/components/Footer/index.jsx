18ef99327e9b43643a404d22a33bc49abe9dc52f
Thiago <thiagodsantos316@gmail.com>
2023-01-15 11:14:46 -0300

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,23 @@
+import { Link } from "react-router-dom";
 import "./Styles.css";
+import Pin from '../../assets/image/pin.svg'
+import Phone from '../../assets/image/phone.svg'
+import Social from '../../assets/image/redes-sociais-icon.svg'
+import Nauber from '../../assets/image/logo-nauber@2x.png'
 
 function Footer() {
   return (
-
+    <>
       <div className="frame-div4">
       <div className="group-div">
         <div className="group-div">
           <div className="nauber-pro-div">
             <div className="bg-div" />
-            <img className="phone-icon" alt="" src="src\assets\image\phone.svg" />
+            <img 
+                className="phone-icon" 
+                alt="" 
+                src={Phone} 
+            />
             <div className="siga-nas-redes-sociais">
               Siga nas redes sociais
             </div>
@@ -24,7 +33,7 @@ function Footer() {
             <img
               className="redes-sociais-icon"
               alt=""
-              src="src\assets\image\redes-sociais-icon.svg"
+              src={Social}
             />
             <div className="dvidas-div">
               <div className="converse-com-um-especialista">
@@ -35,8 +44,8 @@ function Footer() {
               <div className="ficou-com-dvidas">Ficou com dúvidas?</div>
             </div>
             <div className="produtos-div1">
-              <div className="construo-civil-div">Construção civil</div>
-              <div className="linha-industrial-div">Linha industrial</div>
+              <Link to='/Categorias' className="construo-civil-div">Construção civil</Link>
+              <Link to='' className="linha-industrial-div">Linha industrial</Link>
               <div className="ficou-com-dvidas">Produtos</div>
             </div>
             <div className="nossa-sede-div">
@@ -51,18 +60,23 @@ function Footer() {
               <div className="div2">+55 (47) 98821-2725</div>
               <div className="nossa-sede-div1">Nossa sede</div>
             </div>
-            <img className="pin-icon" alt="" src="src\assets\image\pin.svg" />
+            <img 
+                className="pin-icon" 
+                alt="" 
+                src={Pin} 
+            />
             <div className="line-div1" />
             <img
               className="logo-nauber-icon"
               alt=""
-              src="src\assets\image\logo-nauber@2x.png"
+              src={Nauber}
             />
             <div className="rectangle-div6" />
           </div>
         </div>
       </div>
     </div>
+    </>
 
 );
 };
