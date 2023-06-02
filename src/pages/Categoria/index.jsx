ed92eb05d7f0aea9c7d94a00760d9d66e2b594ad
Thiago <thiagodsantos316@gmail.com>
2023-06-01 22:48:33 -0300

    Update index.jsx

--- src/pages/Categoria/index.jsx
+++ src/pages/Categoria/index.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import { useTranslation } from "react-i18next";
