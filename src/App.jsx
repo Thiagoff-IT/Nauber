fe2e8fa96eb3ac1108b713fba84d517241a49718
Thiago <thiagodsantos316@gmail.com>
2023-06-12 21:58:51 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,77 @@
 import react from 'react'
-import Home from './pages/Home'
+import {
+  createBrowserRouter
+} from "react-router-dom";
+
 import './Styles/global.css'
 
-function App() {
-  return (
-    <Home />
-  )
-}
+import Home from './pages/Home';
+import Produto from './pages/Produtos';
+import Assistencia from './pages/Assistencia';
+import Sobre from './pages/Sobre'
+import Contato from './pages/Contato';
+import Catalogos from './pages/Catalogos';
+import Categoria from './pages/Categoria';
+import Categorias from './pages/Categorias';
+import ProdutoCompras from './pages/ProdutoCompra';
+import ProdutoLogistico from './pages/ProdutoLogistas';
+import ProdutoDetalhes from "./pages/ProdutoDetalhes"
+import CategoriasDeProdutos from "./pages/CategoriasDeProdutos"
+import Login from './pages/Login'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/Produto",
+    element: <Produto />,
+  },
+  {
+    path: "/Assistencia",
+    element: <Assistencia />,
+  },
+  {
+    path: "/Sobre",
+    element: <Sobre />,
+  },
+  {
+    path: "/Contato",
+    element: <Contato />,
+  },
+  {
+    path: "/Catalogos",
+    element: <Catalogos />,
+  },
+  {
+    path: "/Categoria",
+    element: <Categoria />,
+  },
+  {
+    path: "/Categoria/CategoriasDeProdutos",
+    element: <CategoriasDeProdutos />,
+  },
+  {
+    path: "/Categoria/Categorias",
+    element: <Categorias />,
+  },
+  {
+    path: "/ProdutoLogistico",
+    element: <ProdutoLogistico />,
+  },
+  {
+    path: "/ProdutoDetalhes",
+    element: <ProdutoDetalhes />,
+  },
+  {
+    path: "/ProdutoCompra",
+    element: <ProdutoCompras />,
+  },
+  {
+    path: "/Login",
+    element: <Login />,
+  },
+]);
 
 export default App
